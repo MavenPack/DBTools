8ab3b30c4d4f404af71b346796aea42321ecfb1e
yeild_eclipse <935057137@qq.com>
2017-04-02 16:19:10 +0800

    Signed-off-by: yeild_eclipse <935057137@qq.com>

--- src/main/java/com/yeild/common/dbtools/DBConf.java
+++ src/main/java/com/yeild/common/dbtools/DBConf.java
@@ -6,35 +6,64 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.log4j.Logger;
+
 import com.yeild.common.Utils.ConvertUtils;
 
 public class DBConf {
+	private static Logger logger = Logger.getLogger(DBConf.class);
 	public static String dbConfName="db.properties";
 	public static String dbConfPath=null;
+	public static String dbDefConfPath=null;
+    private static Properties dbDefProperties = null;
     private static Properties dbProperties = null;
     public static String dbPoolUsed="proxool";
     public static String dbNameDefault="postgre";
 
     public static void loadDbConfig(String confPath) throws IOException {
+    	if(dbDefConfPath == null) {
+    		InputStream inputStream = DBConf.class.getClassLoader().getResourceAsStream(dbConfName);
+    		if(inputStream == null) {
+    			throw new IOException("default db config file not found.");
+    		}
+    		dbDefProperties = new Properties();
+    		dbDefProperties.load(inputStream);
+    	}
+    	if(dbDefProperties == null) {
+    		dbDefProperties = getProperties(dbDefConfPath);
+    	}
     	dbConfPath = confPath;
-    	InputStream confInputStream = null;
-		try {
-			File confFile = new File(dbConfPath + dbConfName);
-			confInputStream = new FileInputStream(confFile);
-			dbProperties = new Properties();
-			dbProperties.load(confInputStream);
-			dbPoolUsed = dbProperties.getProperty("database.pool", "proxool");
-			dbNameDefault = dbProperties.getProperty("database.default", "postgre");
-		} catch (IOException e) {
-			throw e;
+    	FileInputStream confInputStream = null;
+    	try {
+    		File confFile = new File(dbConfPath + dbConfName);
+    		confInputStream = new FileInputStream(confFile);
+    		dbProperties = new Properties();
+    		dbProperties.load(confInputStream);
+    	} catch (IOException e) {
+    		logger.debug("Could not find config file for path " + dbConfPath + dbConfName);
+    		logger.debug("Use default db config.");
 		} finally {
-			if (confInputStream != null) {
-				try {
+			if(confInputStream != null) {
+				try{
 					confInputStream.close();
-				} catch (IOException e) {
-				}
+				} catch (Exception e2) {}
 			}
 		}
+		dbPoolUsed = getConfValue("database.pool", "proxool");
+		dbNameDefault = getConfValue("database.default", "postgre");
+    }
+    
+    private static Properties getProperties(String path) throws IOException {
+		File confFile = new File(path);
+		FileInputStream confInputStream = new FileInputStream(confFile);
+		Properties result = new Properties();
+		result.load(confInputStream);
+		confInputStream.close();
+		return result;
+    }
+    
+    private static String getConfValue(String key, String defValue) {
+    	return dbProperties == null ? dbDefProperties.getProperty(key, defValue) : dbProperties.getProperty(key, dbDefProperties.getProperty(key, defValue));
     }
     
     public static String getDbConf(String name) {
@@ -50,10 +79,7 @@ public class DBConf {
     }
     
     public static String getDbConfWithName(String dbname, String name, String defaultValue) {
-        if (dbProperties == null) {
-            return defaultValue;
-        }
-        return dbProperties.getProperty(dbname+"."+name, defaultValue);
+        return getConfValue(dbname+"."+name, defaultValue);
     }
     
     public static String getDbPoolConf(String name) {
@@ -61,10 +87,7 @@ public class DBConf {
     }
     
     public static String getDbPoolConf(String name, String defaultValue) {
-        if (dbProperties == null) {
-            return defaultValue;
-        }
-        return dbProperties.getProperty(dbPoolUsed+"."+name, defaultValue);
+        return getConfValue(dbPoolUsed+"."+name, defaultValue);
     }
     public static int getDbPoolConf(String name, int defaultValue) {
         return ConvertUtils.parseInt(getDbPoolConf(name), defaultValue);
--- src/main/java/com/yeild/common/dbtools/database/DbConnectionManager.java
+++ src/main/java/com/yeild/common/dbtools/database/DbConnectionManager.java
@@ -1,5 +1,6 @@
 package com.yeild.common.dbtools.database;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
